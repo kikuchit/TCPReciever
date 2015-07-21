0dfb447303ff0ffec7883f388ca5a354950a0bfe
kikuchit <takashi.kikuchi.0914@gmail.com>
2015-07-21 18:03:24 +0900

    初回コミット

---
+++ TCPReciever/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCPReciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TCPReciever recv = new TCPReciever("127.0.0.1", 15000);
+            recv.listen();
+        }
+    }
+}
---
+++ TCPReciever/TCPReciever.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPReciever
+{
+    class TCPReciever
+    {
+        string server;
+        int port;
+        TcpListener listener;
+
+        public TCPReciever(string server, int port)
+        {
+            this.server = server;
+            this.port = port;
+            this.listener = null;
+        }
+
+        public void listen()
+        {
+            try
+            {
+                IPAddress addr = IPAddress.Parse(this.server);
+                this.listener = new TcpListener(addr, this.port);
+
+                this.listener.Start();
+
+                byte[] buf = new byte[1024];
+                string message = null;
+
+                while (true)
+                {
+                    Console.WriteLine("接続待ち...");
+                    using (TcpClient client = this.listener.AcceptTcpClient())
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        Console.WriteLine("接続完了！");
+                        int i;
+                        while ((i = stream.Read(buf, 0, buf.Length)) != 0)
+                        {
+                            message = System.Text.Encoding.ASCII.GetString(buf, 0, i);
+                            Console.WriteLine("受信文字列：{0}", message);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                this.listener.Stop();
+            }
+        }
+    }
+}
